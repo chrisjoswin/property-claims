773a650db2c8ad18a47c2b866d746751de9571e2
Christopher Erattuparambil <chris.joswin@gmail.com>
2024-09-19 22:48:23 -0500

    initial files

---
+++ claim-processing-frontend/src/app/claim-status/claim-status.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-claim-status',
+  standalone: true,
+  imports: [],
+  templateUrl: './claim-status.component.html',
+  styleUrl: './claim-status.component.css'
+})
+export class ClaimStatusComponent {
+
+}
---
+++ claim-processing-frontend/src/app/claim-submission/claim-submission.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-claim-submission',
+  standalone: true,
+  imports: [],
+  templateUrl: './claim-submission.component.html',
+  styleUrl: './claim-submission.component.css'
+})
+export class ClaimSubmissionComponent {
+
+}
