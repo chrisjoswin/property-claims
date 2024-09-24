d2c09311b29ade633d9ce2768a8195005ee3d8aa
Christopher Erattuparambil <chris.joswin@gmail.com>
2024-09-24 12:03:31 -0500

    secret add

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -3,7 +3,7 @@ import os
 from dotenv import load_dotenv
 load_dotenv()
 class Settings(BaseSettings):
-    database_url: str  = os.getenv("MONGO_DB_URI")  
+    database_url: str  = os.environ.get("MONGO_DB_URI")  
     database_name: str = "insurance_claims"
 
 settings = Settings()
